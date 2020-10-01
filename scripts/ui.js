c7bfd8516af4c263e5059a8228c58f475ad4b909
srnogueira <70414058+srnogueira@users.noreply.github.com>
2020-10-01 08:35:43 -0300

    Update ui.js

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,167 +1,124 @@
-// User interface javascript
-
-// Resize textbox
-function calcHeight(value) {
-    let numberOfLineBreaks = (value.match(/\n/g) || []).length;
-    // min-height + lines x line-height + padding + border
-    let newHeight = 20 + numberOfLineBreaks * 24 + 4 +1;
-    return newHeight;
+html {
+    font-size: 12pt;
+    font-family: math;
+    margin:1%;
 }
 
-let textarea = document.querySelector(".box");
-let box = document.querySelector(".solBox");
-
-function autosize(){
-    textarea.style.height = calcHeight(textarea.value) + "px";
+header{
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    margin:0% 0% 1% 0%;
 }
 
-function hide(extra){
-    if (extra.key !== "ArrowRight" && extra.key !== "ArrowLeft" && extra.key !== "ArrowDown" && extra.key !== "ArrowUp"){
-	box.style.display="";
-    }
+header h1{
+    margin:0% 1% 0% 0%;
 }
 
-autosize();
-textarea.addEventListener("keyup", autosize);
-textarea.addEventListener("keydown", hide);
-
-// Help button
-function toggleHelp(){
-    let x = document.querySelector(".helpText");
-    if (x.style.display===""){
-	x.style.display="flex";
-	helpButton.innerText="Help (-)";
-    }
-    else{
-	x.style.display="";
-	helpButton.innerText="Help (+)";
-    }
-}
-const helpButton = document.querySelector(".help");
-helpButton.onclick = toggleHelp;
-
-// Function button
-function toggleFunctions(){
-    let x = document.querySelector(".functionBox");
-    if (x.style.display===""){
-	x.style.display="flex";
-	functionButton.innerText="Functions (-)";
-    }
-    else{
-	x.style.display="";
-	functionButton.innerText="Functions (+)";
-    }
+button{
+    font-family:math;
+    font-size:12pt;
+    padding: 0.5% 2%;
+    border-radius:5px;
+    border:2px solid #3d3d3d;
+    margin-right:10px;
 }
 
-const functionButton = document.querySelector(".function");
-functionButton.onclick = toggleFunctions;
-
-// PropsSI
-function generateFun(){
-    const textBox = document.querySelector(".box");
-    let fluid = document.querySelector(".FluidName");
-    let property = document.querySelector(".Property");
-    let input1 = document.querySelector(".Input1");
-    let input2 = document.querySelector(".Input2");
-    let value1 = document.querySelector(".value1");
-    let value2 = document.querySelector(".value2");
-
-    let fluidName=fluid.options[fluid.selectedIndex].value;
-    let propName=property.options[property.selectedIndex].value;
-    let input1Name=input1.options[input1.selectedIndex].value;
-    let input2Name=input2.options[input2.selectedIndex].value;
-
-    let text = "property=PropsSI('"+propName+"','"+input1Name+"',"+value1.value+",'"+input2Name+"',"+value2.value+",'"+fluidName+"')";
-    textBox.value+="\n"+text;
-    autosize();
-    toggleFunctions();
-}
-const PropsSIButton = document.querySelector(".butPropsSI");
-PropsSIButton.onclick = generateFun;
-
-//HAPropsSI
-function generateFun2(){
-    const textBox = document.querySelector(".box");
-    let property = document.querySelector(".HAProperty");
-    let input1 = document.querySelector(".HAInput1");
-    let input2 = document.querySelector(".HAInput2");
-    let input3 = document.querySelector(".HAInput3");
-    let value1 = document.querySelector(".HAvalue1");
-    let value2 = document.querySelector(".HAvalue2");
-    let value3 = document.querySelector(".HAvalue3");
-
-    let propName=property.options[property.selectedIndex].value;
-    let input1Name=input1.options[input1.selectedIndex].value;
-    let input2Name=input2.options[input2.selectedIndex].value;
-    let input3Name=input2.options[input3.selectedIndex].value;
-
-    let text = "property=HAPropsSI('"+propName+"','"+input1Name+"',"+value1.value+",'"+input2Name+"',"+value2.value+",'"+input3Name+"',"+value3.value+")";
-    textBox.value+="\n"+text;
-    autosize();
-    toggleFunctions();
-}
-const HAPropsSIButton = document.querySelector(".butHAPropsSI");
-HAPropsSIButton.onclick = generateFun2;
-
-//Save a file
-function saveFile()
-{
-    var textToSave = document.getElementById("box").value;
-    var textToSaveAsBlob = new Blob([textToSave], {type:"text/plain"});
-    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
-    
-    var downloadLink = document.createElement("a");
-    var filename = document.getElementById("inputFileNameToSaveAs").value;
-    if (filename===""){
-	downloadLink.download="laine_save.txt";
-    }
-    else{ 
-	downloadLink.download = filename;
-    }
-    downloadLink.innerHTML = "Download File";
-    downloadLink.href = textToSaveAsURL;
-    downloadLink.onclick = destroyClickedElement;
-    downloadLink.style.display = "none";
-    document.body.appendChild(downloadLink);
-    
-    downloadLink.click();
+button:hover{
+    background-color: #F04747;
 }
 
-function destroyClickedElement(event)
-{
-    document.body.removeChild(event.target);
+.helpText,.functionBox,.fileBox {
+    display:none;
+    flex-wrap:wrap;
 }
 
-// Load a file
-function loadFileAsText()
-{
-    var downloadLink = document.createElement("a");
-    
-    var fileToLoad = document.getElementById("fileToLoad").files[0];
-    
-    var fileReader = new FileReader();
-    fileReader.onload = function(fileLoadedEvent) 
-    {
-	var textFromFileLoaded = fileLoadedEvent.target.result;
-	document.getElementById("box").value = textFromFileLoaded;
-	autosize();
-    };
-    fileReader.readAsText(fileToLoad, "UTF-8");
+.solBox{
+    display:none;
 }
 
-
-// File button
-function toggleFile(){
-    let x = document.querySelector(".fileBox");
-    if (x.style.display===""){
-	x.style.display="flex";
-	fileButton.innerText="File (-)";
-    }
-    else{
-	x.style.display="";
-	fileButton.innerText="File (+)";
-    }
+label {
+    color:#F04747;
+    font-weight: 700;
 }
 
-const fileButton = document.querySelector(".file");
-fileButton.onclick = toggleFile;
+.helpText div {
+    max-width: 300px;
+    background-color:#fcec;
+    margin:0.5%;
+    padding: 0.5%;
+}
+
+.functionBox div{
+    max-width: 350px;
+    background-color:#fcec;
+    margin:0.5%;
+    padding: 0.5%;
+}
+
+.fileBox div{
+    min-width: 200px;
+    max-width: 350px;
+    background-color:#fcec;
+    margin:0.5%;
+    padding: 0.5%;
+}
+
+select,input {
+    max-width: 95%;
+}
+
+input{
+    width:90%;
+    margin-bottom:2%;
+}
+
+main {
+    display: flex;
+    flex-wrap: wrap;
+    padding:0.5%;
+}
+
+main div {
+    margin-right: 2%;
+    max-width: 100%;
+}
+
+h2{
+    font-size:14pt;
+    color: #F04747;
+}
+
+main a{
+    font-size:14pt;
+    color: #F04747;
+    padding:1px;
+}
+
+textarea {
+    font-family:math;
+    font-size: 12pt;
+    padding:1px;
+    line-height: 1.5;
+    border-radius: 5px;
+    border: 2px solid #3d3d3d;
+    max-width: 98%;
+    overflow:hidden;
+}
+
+.out{
+    min-height: 24px;
+    border-radius: 5px;
+    border: 2px solid #3d3d3d;
+    padding: 2%;
+    width: 100%;
+    background-color:#fced;
+}
+
+.out p{
+    margin: 5px;
+}
+
+footer {
+    font-size: 10pt;
+}
