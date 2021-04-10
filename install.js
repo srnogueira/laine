2850242b18ea5d0238344760a7733e84df3c6e55
srnogueira <70414058+srnogueira@users.noreply.github.com>
2021-04-10 15:29:03 -0300

    Update install.js

--- install.js
+++ install.js
@@ -3,18 +3,14 @@ self.addEventListener('install', (e) => {
     caches.open('fox-store').then((cache) => cache.addAll([
 	'./',
 	'./index.html',
-	'./img',
 	'./img/laine.png',
-	'./styles',
 	'./styles/style.css',
 	'./styles/codemirror.css',
-	'./scripts',
 	'./scripts/functions.js',
 	'./scripts/laine.js',
 	'./scripts/editor.js',
 	'./scripts/plots.js',
 	'./scripts/ui.js',
-	'./scripts/third-party',
 	'./scripts/third-party/coolprop.js',
 	'./scripts/third-party/coolprop.wasm'
     ])),
