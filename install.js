e89b0a74ea04031696afc63018f62a615ff592f3
srnogueira <rafaelnnakashima@gmail.com>
2021-08-12 13:50:07 -0300

    coolprop license Added the license of coolprop in the repository

--- install.js
+++ install.js
@@ -2,7 +2,7 @@
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches
-      .open("laine-store.22.07.21")
+      .open("laine-store.12.08.21")
       .then((cache) =>
         cache.addAll([
           "./",
@@ -15,8 +15,9 @@ self.addEventListener("install", (e) => {
           "./scripts/plots.js",
           "./scripts/ui.js",
           "./scripts/editor.js",
-          "./scripts/third-party/coolprop.js",
-          "./scripts/third-party/coolprop.wasm",
+          "./scripts/coolprop/coolprop.js",
+          "./scripts/coolprop/coolprop.wasm",
+          "./scripts/coolprop/LICENSE",
         ])
       )
   );
@@ -24,7 +25,7 @@ self.addEventListener("install", (e) => {
 
 // Delete old cache
 self.addEventListener('activate', (event) => {
-  var cacheKeeplist = ['laine-store.22.07.21'];
+  var cacheKeeplist = ['laine-store.12.08.21'];
   event.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
